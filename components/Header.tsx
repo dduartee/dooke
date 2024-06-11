3d22df126fb682a87e436c190c09ab5728cb1f8e
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2024-06-11 20:47:02 +0000

    acesso a plataforma pelo menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ import { HeaderMenu } from "./Menu";
 export default function DookeHeader() {
   return (
     <Box height={"70px"} bg={"brand.500"} padding={3} display={"flex"} color={"#fff"} justifyContent={"space-around"} alignItems={"center"}>
-      <Image src={"/logo.png"} height={"50px"}/>
-      <Box display={["none", "flex"]} justifyContent={"space-around"} alignItems={"center"} width={"300px"}>
+      <Image src={"/logo.png"} height={"50px"} alt="Logo Dooke"/>
+      <Box display={["none", "flex"]} justifyContent={"space-around"} alignItems={"center"} width={"400px"}>
         <Link as={NextLink} href='/'>
           <Text fontSize={"1.4rem"}>Home</Text>
         </Link>
@@ -15,6 +15,9 @@ export default function DookeHeader() {
         </Link>
         <Link as={NextLink} href='#about'>
           <Text fontSize={"1.4rem"}>Sobre nós</Text>
+        </Link>
+        <Link as={NextLink} href='/acesso'>
+          <Text fontSize={"1.4rem"}>Acesso</Text>
         </Link>
       </Box>
       <Box display={["flex", "none"]} position={"absolute"} right={0} mr={10} >
--- pages/plataforma.tsx
+++ pages/plataforma.tsx
@@ -1,29 +1,68 @@
 import { Box, Image, Link } from "@chakra-ui/react";
-import { Divider } from '@chakra-ui/react'
+import { Divider } from "@chakra-ui/react";
 export function DookeSidebar() {
-    return (
-        <Box position={"fixed"} left={0} top={0} padding={"2rem"} bgColor={"#fff"} height={"100vh"} width={"200px"}
-            display={"flex"} flexDirection={"column"}>
-            <Image src={"/dookeEducation.png"} mb={"1rem"} />
-            <Divider mb={"2rem"} />
-            <Box display={"flex"} flexDirection={"column"} justifyContent={"space-between"} height={"100%"}>
-                <Box display={"flex"} flexDirection={"column"} textAlign={"center"}>
-                    <Link mb={"1rem"}>Dashboard</Link>
-                    <Link mb={"1rem"}>Turmas</Link>
-                    <Link mb={"1rem"}>Alunos</Link>
-                    <Link mb={"1rem"}>Professores</Link>
-                    <Link mb={"1rem"}>Mensagens</Link>
-                    <Link mb={"1rem"}>Notificações</Link>
-                    <Link mb={"1rem"}>Configurações</Link>
-                </Box>
-                <Link mb={"1rem"} href="/acesso">Sair</Link>
-            </Box>
+  return (
+    <Box
+      padding={"1rem"}
+      bgColor={"#fff"}
+      height={"100vh"}
+      width={"200px"}
+      display={"flex"}
+      flexDirection={"column"}
+    >
+      <Image src={"/dookeEducation.png"} mb={"1rem"} />
+      <Divider mb={"2rem"} />
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+        height={"100%"}
+      >
+        <Box display={"flex"} flexDirection={"column"} textAlign={"center"}>
+          <Link mb={"1rem"}>Início</Link>
+          <Link mb={"1rem"}>Turmas</Link>
+          <Link mb={"1rem"}>Alunos</Link>
+          <Link mb={"1rem"}>Professores</Link>
+          <Link mb={"1rem"}>Mensagens</Link>
+          <Link mb={"1rem"}>Notificações</Link>
+          <Link mb={"1rem"}>Configurações</Link>
         </Box>
-    )
-}
-
-export default function Plataforma() {
-    return <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} mb={"5rem"} height={"100vh"}>
-        <DookeSidebar />
+        <Link mb={"1rem"} href="/acesso">
+          Sair
+        </Link>
+      </Box>
     </Box>
+  );
+}
+// inicio: página amigável que mostra um overview das matérias de robotica e atividades com a porcentagem de conclusão
+export function DookeContent() {
+  return (
+    <Box  bgColor={"#fff"} margin={"2rem"} width={"100%"}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        padding={"1rem"}
+      >
+        <Box>
+          <h1>Olá, Fulano!</h1>
+          <p>Seja bem-vindo ao Dooke Education</p>
+        </Box>
+    </Box>
+    </Box>
+    );
+}
+export default function Plataforma() {
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"row"}
+      mb={"5rem"}
+      height={"100vh"}
+    >
+      <DookeSidebar />
+      <DookeContent />
+    </Box>
+  );
 }
